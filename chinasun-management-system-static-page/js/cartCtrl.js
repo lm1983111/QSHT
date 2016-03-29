aa48e08f902291e9b2f8db399845638d5fb527b3
lm1983111 <315548007@qq.com>
2016-03-29 17:02:22 +0800

    post订单

--- chinasun-management-system-static-page/js/cartCtrl.js
+++ chinasun-management-system-static-page/js/cartCtrl.js
@@ -92,4 +92,39 @@ myApp.controller('cartCtrl',['$scope','$http',function($scope,$http){
         });
         return $scope.allprice;
     };
+
+    //post数据到服务器
+    $scope.postOrder = function() {
+        if($scope.allprice > 0){
+            //console.log($scope.dataList);  //打印改变后的json数据流
+            //console.log($scope.allprice);  //打印生成的总价格
+
+            //先定义一个空数组，获取改变了的数据items,num插入到这个空数组（这个数组就是将要传给服务器的数据）
+            var arrayPostOrderObj = new Array();
+            angular.forEach($scope.dataList,function(data,index,array){
+                if(data.Bol == true){  //循环每一项，如果该项被选择
+                    //console.log(data.Bol);  //打印该项的Bol
+                    //console.log(data.items);  //打印该项的items
+                    //console.log(data.num);  //打印该项改变后的num
+
+                    arrayPostOrderObj.push(data.Bol,data.num,data.items);
+                    console.log(arrayPostOrderObj);
+                }
+            });
+
+            //上传数据
+            /*$http({
+                method:'post',
+                url:'post.php',
+                data:$scope.dataList
+            }).success(function(){
+                alert('数据上传成功')
+            }).error(function(){
+                alert('上传失败')
+            })*/
+        }else{
+            return false;
+        }
+    };
+    //post数据到服务器结束
 }]);
