0a22471f242147461108acea444bc009c41c1134
lm1983111 <315548007@qq.com>
2016-04-11 22:32:24 +0800

    大图脚本

--- chinasun-management-system-static-page/js/common.js
+++ chinasun-management-system-static-page/js/common.js
@@ -52,16 +52,13 @@ function validateTele(event){
 }
 
 
-//点击缩略图显示大图
+//点击缩略图显示大图函数
 function showSourceImg(event){
     var thisImgEle = $(event.target);
     var sourceUrl = $(thisImgEle).attr('src');
-    console.log(sourceUrl);
     //获取页面尺寸
     var wWidthShowImg = $(window).width();
     var wHeightShowImg = $(window).height();
-    console.log(wWidthShowImg);
-    console.log(wHeightShowImg);
     //创建一个新的img对象，并获取图像宽高
     var sourceImgObj = new Image();
     sourceImgObj.src = sourceUrl;
@@ -69,13 +66,21 @@ function showSourceImg(event){
     var imgHeightNatural = sourceImgObj.height;
     console.log(imgWidthNatural);
     console.log(imgHeightNatural);
-    //点击按钮弹出层，同时在body底部加入mask
-
-
-    /*$('body').append('<div class="source-img-wrap">' +
-        '<img src="' +
-        sourceUrl +
-        '"> ' +
-        '</div>');*/
-
+    //点击缩略图弹出大图，同时在body底部加入mask
+    $('body').append('<div class="source-img-wrap">' +
+        '<span class="close-source-img" onclick="closeSourceImg()">×</span>' +
+        '<img src="' + sourceUrl +
+        '">' +
+        '</div>' +
+        '<div class="mask"></div>');
+    if(imgWidthNatural > 800 || imgHeightNatural > 500){
+        $('.source-img-wrap').css({"width":"800px","height":"500px","margin-left":"-400px","margin-top":"-250px"});
+    }else{
+        $('.source-img-wrap').css({"width":imgWidthNatural,"height":imgHeightNatural,"margin-left":-(imgWidthNatural/2),"margin-top":-(imgHeightNatural/2)});
+    }
+}
+//关闭大图与mask函数
+function closeSourceImg(){
+    $('.source-img-wrap').remove();
+    $('.mask').remove();
 }
