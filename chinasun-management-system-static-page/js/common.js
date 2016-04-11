2ceb2ab5d25b31d7f0fc98c0547e107754e43b06
lm1983111 <315548007@qq.com>
2016-04-11 17:10:33 +0800

    新增实名认证

--- chinasun-management-system-static-page/js/common.js
+++ chinasun-management-system-static-page/js/common.js
@@ -1,4 +1,11 @@
 $(function(){
+    //进页面给页面添加高宽
+    (function setBodyWithHeight(){
+        var wWith = $(window).width();
+        var wHeight = $(window).height();
+        $(".view-body").css({"min-height":wHeight-80});
+    })();
+
     //获取并添加左侧sidebar-trans高度，为了添加height0过度动画
     $(".sidebar-trans").each(function(){
         var height = $(this).height();
@@ -29,3 +36,46 @@ function heightReply(){
     }
 }
 
+
+
+//手机号校验
+function validateTele(event){
+    if(!(/^1[3|4|5|7|8]\d{9}$/.test(event.target.value))){
+        //console.log(event.target.value)
+        var ThisEleParent  = event.target.parentNode;  //插入提示的父亲容器
+        $(event.target).next(".red-note").html("");
+        $(event.target).next(".red-note").html("手机号格式不正确!");
+        return false;
+    }else{
+        $(event.target).next(".red-note").html("");
+    }
+}
+
+
+//点击缩略图显示大图
+function showSourceImg(event){
+    var thisImgEle = $(event.target);
+    var sourceUrl = $(thisImgEle).attr('src');
+    console.log(sourceUrl);
+    //获取页面尺寸
+    var wWidthShowImg = $(window).width();
+    var wHeightShowImg = $(window).height();
+    console.log(wWidthShowImg);
+    console.log(wHeightShowImg);
+    //创建一个新的img对象，并获取图像宽高
+    var sourceImgObj = new Image();
+    sourceImgObj.src = sourceUrl;
+    var imgWidthNatural = sourceImgObj.width;
+    var imgHeightNatural = sourceImgObj.height;
+    console.log(imgWidthNatural);
+    console.log(imgHeightNatural);
+    //点击按钮弹出层，同时在body底部加入mask
+
+
+    /*$('body').append('<div class="source-img-wrap">' +
+        '<img src="' +
+        sourceUrl +
+        '"> ' +
+        '</div>');*/
+
+}
