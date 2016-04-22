35313d3a97fa135757243248788d4c9e31610a58
lm1983111 <315548007@qq.com>
2016-04-22 17:11:09 +0800

    j

--- chinasun-management-system-static-page/js/common.js
+++ chinasun-management-system-static-page/js/common.js
@@ -1,10 +1,15 @@
 $(function(){
     //进页面给页面添加高宽
-    (function setBodyWithHeight(){
+    function setBodyWithHeight(){
         var wWith = $(window).width();
         var wHeight = $(window).height();
         $(".view-body").css({"min-height":wHeight-80});
-    })();
+        $(".wizard").css({"min-height":wHeight-140})
+    }
+    setBodyWithHeight();
+    $(window).resize(function(){
+        setBodyWithHeight()
+    });
 
     //获取并添加左侧sidebar-trans高度，为了添加height0过度动画
     $(".sidebar-trans").each(function(){
@@ -49,38 +54,4 @@ function validateTele(event){
     }else{
         $(event.target).next(".red-note").html("");
     }
-}
-
-
-//点击缩略图显示大图函数
-function showSourceImg(event){
-    var thisImgEle = $(event.target);
-    var sourceUrl = $(thisImgEle).attr('src');
-    //获取页面尺寸
-    var wWidthShowImg = $(window).width();
-    var wHeightShowImg = $(window).height();
-    //创建一个新的img对象，并获取图像宽高
-    var sourceImgObj = new Image();
-    sourceImgObj.src = sourceUrl;
-    var imgWidthNatural = sourceImgObj.width;
-    var imgHeightNatural = sourceImgObj.height;
-    console.log(imgWidthNatural);
-    console.log(imgHeightNatural);
-    //点击缩略图弹出大图，同时在body底部加入mask
-    $('body').append('<div class="source-img-wrap">' +
-        '<span class="close-source-img" onclick="closeSourceImg()">×</span>' +
-        '<img src="' + sourceUrl +
-        '">' +
-        '</div>' +
-        '<div class="mask"></div>');
-    if(imgWidthNatural > 800 || imgHeightNatural > 500){
-        $('.source-img-wrap').css({"width":"800px","height":"500px","margin-left":"-400px","margin-top":"-250px"});
-    }else{
-        $('.source-img-wrap').css({"width":imgWidthNatural,"height":imgHeightNatural,"margin-left":-(imgWidthNatural/2),"margin-top":-(imgHeightNatural/2)});
-    }
-}
-//关闭大图与mask函数
-function closeSourceImg(){
-    $('.source-img-wrap').remove();
-    $('.mask').remove();
 }
